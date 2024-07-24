86b5e896dafbf613d895173aca7cd67a0b57eaa7
Franco Guglielmelli <gugl0010@algonquinlive.com>
2024-07-24 18:35:48 -0300

    hi everyone its me

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = "mongodb://fitflex:qnyrpsKDUTcYBUKE68sosUuU355aZYh1euwilp5K9lM9vZX4nJeMVv0Uaxh7YapzaqIYHtuR6jLvACDbGXceaA==@fitflex.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@fitflex@";
-
+const uri = process.env.COSMOS_DB_CONNECTION_STRING;
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
